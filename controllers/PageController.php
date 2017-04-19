1a34c02d7edba681697aa1ec2f4876581e8ca63b
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-04-19 11:56:47 -0500

    resolving conflict

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -24,6 +24,7 @@ function pageController()
         case '/login': 
             $mainView = '../views/users/login.php';
             $data['message'] = "";
+
             if (Auth::check()) {
                     header("Location: /");
                     exit;
