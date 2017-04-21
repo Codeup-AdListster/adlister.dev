5da93de969bbe19a6056222ddbe50177096f9dae
Thomas Hayes <thomashayes7889@gmail.com>
2017-04-21 13:58:14 -0500

    cleaned up login/signup section

--- views/ads/show.php
+++ views/ads/show.php
@@ -1,7 +1,7 @@
 <!--Page for single advertisement -->
 <div class="container">
 	<div class="col-xs-6">
-		<p> <?= $ad->title ?></p>
+		<h1> <?= $ad->title ?></h1>
 		<p><img src="<?= $ad->image ?>"></p>
 		<p>$ <?= $ad->price ?></p>	
 		<p> <?= $ad->description ?></p>
